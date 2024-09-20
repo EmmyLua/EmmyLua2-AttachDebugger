026d5ceea7f64445f182eb3fac1994d4b734f3ce
CppCXY <812125110@qq.com>
2024-09-20 15:10:05 +0800

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,20 +23,54 @@ plugins {
     id("de.undercouch.download").version("5.3.0")
 }
 
+data class BuildData(
+    val ideaSDKShortVersion: String,
+    // https://www.jetbrains.com/intellij-repository/releases
+    val ideaSDKVersion: String,
+    val sinceBuild: String,
+    val untilBuild: String,
+    val archiveName: String = "EmmyLua2-AttachDebugger",
+    val jvmTarget: String = "17",
+    val targetCompatibilityLevel: JavaVersion = JavaVersion.VERSION_17,
+    // https://github.com/JetBrains/gradle-intellij-plugin/issues/403#issuecomment-542890849
+    val instrumentCodeCompilerVersion: String = ideaSDKVersion,
+    val type: String = "IU"
+)
+
+val buildDataList = listOf(
+    BuildData(
+        ideaSDKShortVersion = "242",
+        ideaSDKVersion = "2024.2",
+        sinceBuild = "232",
+        untilBuild = "242.*",
+    )
+)
+
+group = "com.cppcxy"
+val emmyluaDebuggerVersion = "1.8.2"
+val emmyluaDebuggerProjectUrl = "https://github.com/EmmyLua/EmmyLuaDebugger"
+
+val buildVersion = System.getProperty("IDEA_VER") ?: buildDataList.first().ideaSDKShortVersion
+
+val buildVersionData = buildDataList.find { it.ideaSDKShortVersion == buildVersion }!!
+
+val runnerNumber = System.getenv("RUNNER_NUMBER") ?: "Dev"
+
+version = "${emmyluaDebuggerVersion}.${runnerNumber}-IDEA${buildVersion}"
+
 repositories {
     mavenCentral()
 }
 
 intellij {
     pluginName.set("EmmyLua2-AttachDebugger")
-    version.set("2023.3")
-    type.set("IC") // Target IDE Platform
-    sandboxDir.set("${project.buildDir}/idea-sandbox")
+    version.set(buildVersionData.ideaSDKVersion)
+    type.set(buildVersionData.type) // Target IDE Platform
+    sandboxDir.set("${project.buildDir}/${buildVersionData.ideaSDKShortVersion}/idea-sandbox")
     plugins.set(listOf("com.cppcxy.Intellij-EmmyLua:0.7.1.20-IDEA242"))
 }
 
-val emmyluaDebuggerVersion = "1.8.2"
-val emmyluaDebuggerProjectUrl = "https://github.com/EmmyLua/EmmyLuaDebugger"
+
 
 task("downloadDebugger", type = Download::class) {
     src(arrayOf(
@@ -71,15 +105,18 @@ tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
-        targetCompatibility = "17"
+        targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = buildVersionData.jvmTarget
+    }
+    instrumentCode {
+        compilerVersion.set(buildVersionData.instrumentCodeCompilerVersion)
     }
 
     patchPluginXml {
-        sinceBuild.set("233")
-        untilBuild.set("242.*")
+        sinceBuild.set(buildVersionData.sinceBuild)
+        untilBuild.set(buildVersionData.untilBuild)
     }
 
     signPlugin {
